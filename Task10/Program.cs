48ae8bcbcbe4d2df844ad6b84c2230672c0b6254
Pavel <pavel@b2profit.ru>
2023-10-18 19:08:28 +0300

    Добавил проверку на кол знаков

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.WriteLine("Введите трёхзначное число");
+int number = Convert.ToInt32 (Console.ReadLine());
+if (number < 100 && number > 999)
+{
+    Console.WriteLine("Это не трёхзначное число");
+}
+else
+{
+    int result = (number - (number / 100 * 100)) / 10;
+    Console.WriteLine(result);
+}
