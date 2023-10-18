3bad222b25d1938986d004b1301cd9cce5893bf4
Pavel <pavel@b2profit.ru>
2023-10-18 23:55:06 +0300

    Улучшил 13 Задачу

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -5,31 +5,39 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+int digitNumber(int number)
+{
+int res = 1;  
+while (number >= 10)
+     {
+        number = number / 10;
+        res = res * 10;
+     }
+return res;
+}
+
+
+int thirdNumber(int num)
+{
+int count = 0;  
+while (count < 2)
+  {
+  num = num % digitNumber(num);
+  count = count + 1;
+  }
+return num;
+}
+
+
 Console.WriteLine("Введите число");
 int number = Convert.ToInt32 (Console.ReadLine());
 number = Math.Abs(number);
-int digitNumber = 1;
-int number1 = number;
-int count = 0;
 if (number < 100)
 {
     Console.WriteLine("третьей цифры нет");
 }
 else
-{
-     while (number1 >= 10)
-     {
-        number1 = number1 / 10;
-        digitNumber = digitNumber * 10;
-     }
-       
-
-while (count < 2)
-{
-number = number % digitNumber;
-digitNumber = digitNumber / 10;
-count = count + 1;
-}
-int result = number / digitNumber;
-Console.WriteLine(result);
+{      
+int digit = thirdNumber(number) / digitNumber(thirdNumber(number));
+Console.WriteLine(digit);
 }
